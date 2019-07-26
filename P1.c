20d7624a8d20771e3d9857ba92098e4b6b9346d3
AbhinandanRoul <53302086+AbhinandanRoul@users.noreply.github.com>
2019-07-26 17:54:43 +0530

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+   int i,j;
+   int n,pa,max=0;
+   int a=0,s=0;
+
+   for(i=100;i<=999;i++)
+   {
+       for(j=100;j<=i;j++)
+       {
+           pa=i*j;
+
+           n=pa;
+           while(n>=0)
+           {
+               a=n%10;
+               s=s*10+a;
+               n=n/10;
+           }
+           if(s==pa)//check palindrome
+           {
+               if(pa>max)
+               max=pa;
+           }
+       }
+   }
+   printf("%d",max);
+}
