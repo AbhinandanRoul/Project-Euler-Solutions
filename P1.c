de84990642a5b59bb35773aea913b1c6feef7766
AbhinandanRoul <53302086+AbhinandanRoul@users.noreply.github.com>
2019-08-03 23:34:47 +0530

    Update P1.c

--- P1.c
+++ P1.c
@@ -2,29 +2,13 @@
 
 int main()
 {
-   int i,j;
-   int n,pa,max=0;
-   int a=0,s=0;
+   int i=1,s=0;
 
-   for(i=100;i<=999;i++)
-   {
-       for(j=100;j<=i;j++)
-       {
-           pa=i*j;
-
-           n=pa;
-           while(n>=0)
-           {
-               a=n%10;
-               s=s*10+a;
-               n=n/10;
-           }
-           if(s==pa)//check palindrome
-           {
-               if(pa>max)
-               max=pa;
-           }
-       }
-   }
-   printf("%d",max);
+while(i<=1000)
+{
+   if(i%3==0||i%5==0)
+      s=s+i;
 }
+   printf("%d",s);
+}
+      
